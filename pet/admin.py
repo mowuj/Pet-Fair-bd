73d5b74dbd5dfe3ef869bc11f56985812589436c
mowuj <mowuj92@gmail.com>
2024-01-18 00:17:12 +0600

    pet model

--- pet/admin.py
+++ pet/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import Category,Pet,Review
 # Register your models here.
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'slug']
+
+admin.site.register(Category,CategoryModelAdmin)
+
+class PetModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price','description','image']
+
+    def category(self, obj):
+        return obj.category.name
+admin.site.register(Pet,PetModelAdmin)
+
+
+class ReviewModelAdmin(admin.ModelAdmin):
+    list_display = ['reviewer', 'pet_name', 'rating', 'body', 'timestamps']
+
+    def reviewer(self, obj):
+        return obj.user.username
+    
+    def pet_name(self, obj):
+        return obj.pet.name
+
+admin.site.register(Review,ReviewModelAdmin)
--- pet/models.py
+++ pet/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from . constants import RATING
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    slug=models.SlugField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    category=models.ManyToManyField(Category,related_name='pets')
+    price = models.DecimalField(decimal_places=2,max_digits=12,default=0)
+    image = models.ImageField(upload_to='pet/images',null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Review(models.Model):
+    user = models.ForeignKey(
+        User, related_name="reviews", on_delete=models.CASCADE)
+    pet = models.ForeignKey(
+        Pet, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=RATING)
+    body = models.TextField()   
+    models.DateTimeField(auto_now_add=True)
+    timestamps = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
