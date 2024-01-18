aad4e7193001a5767296f204d0d7652fa496d247
mowuj <mowuj92@gmail.com>
2024-01-19 00:01:15 +0600

    email verification done

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserProfileView
+from .views import UserRegistrationView, UserLoginView, UserProfileView,activate
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('active/<uid64>/<token>/',activate, name='activate'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -14,19 +14,51 @@ from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.template.loader import render_to_string
 from transaction.models import Transaction
 from transaction.constants import BUY
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from .models import Customer
+from django.contrib.auth.models import User
 
 class UserRegistrationView(FormView):
-    template_name='customer/register.html'
-    success_url=reverse_lazy('login')
-    form_class=UserRegistrationForm
+    template_name = 'customer/register.html'
+    success_url = reverse_lazy('login')
+    form_class = UserRegistrationForm
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
+
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        confirm_link = f"http://127.0.0.1:8000/customer/active/{uid}/{token}"
+        email_subject="Confirm Your Email"
+        email_body=render_to_string('customer/confirm_mail.html',{'confirm_link':confirm_link})
+        email=EmailMultiAlternatives(email_subject,'',to=[user.email])
+        email.attach_alternative(email_body,"text/html")
+        email.send()
+
         messages.success(
-            self.request, "Welcome! You have registered Successfully")
+            self.request, "Welcome! You have registered Successfully. Check your email to confirm your account."
+        )
+
+        login(self.request, user)
+
         return super().form_valid(form)
-    
+
+
+def activate(request, uid64, token):
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        user = User._default_manager.get(pk=int(uid))
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('profile')
+    else:
+        return redirect('login')
 class UserLoginView(LoginView):
     template_name='customer/login.html'
 
