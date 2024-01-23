d34a8cf78fb7870d8a87cf8db178178653de8c00
mowuj <mowuj92@gmail.com>
2024-01-24 00:17:51 +0600

    add link

--- customer/views.py
+++ customer/views.py
@@ -36,7 +36,7 @@ class UserRegistrationView(FormView):
         
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
-        confirm_link = f"http://127.0.0.1:8000/customer/active/{uid}/{token}"
+        confirm_link = f"https://pet-fair-bd.onrender.com/customer/active/{uid}/{token}"
         email_subject="Confirm Your Email"
         email_body=render_to_string('customer/confirm_mail.html',{'confirm_link':confirm_link})
         email=EmailMultiAlternatives(email_subject,'',to=[user.email])
